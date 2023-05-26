bf53f2fd1a8031133b7d65224da67f7d846e7239
Tanaya <desaitanaya17@gmail.com>
2023-05-26 19:32:24 +0530

    [Tanaya] Add : C# Program to implement HashTable.

---
+++ HashTable/HashTable/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+
+    public class Node
+    {
+        public string Data { get; }
+        public Node next { get; set; }
+
+        public Node(string data)
+        {
+            Data=data;
+            next = null;
+        }
+    }
+}
