b29063657e61537e450cc72a6f006b45c9f4e16d
Tanaya <desaitanaya17@gmail.com>
2023-05-26 19:32:17 +0530

    [Tanaya] Add : C# Program to implement HashTable.

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HashTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Enter the size : ");
+            size = int.Parse(Console.ReadLine());
+
+            HashTableProgram hashTableProgram = new HashTableProgram(size);
+
+            hashTableProgram.Add("Hello");
+            hashTableProgram.Add("World");
+            hashTableProgram.Add("New");
+            hashTableProgram.Add("Friend");
+            hashTableProgram.Display();
+
+            hashTableProgram.Search("New");
+
+            hashTableProgram.Remove("World");
+            hashTableProgram.Display();
+
+        }
+    } 
+}
